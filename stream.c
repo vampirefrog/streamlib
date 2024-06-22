f245e9b33c73c49cefa1df469466c4decd852f1d
vampi <vampi@vampi.tech>
2024-06-22 15:13:52 +0300

    each_filew: wide char version each_file

--- stream.c
+++ stream.c
@@ -506,6 +506,23 @@ static int each_file_dir(const char *path, struct file_type_filter *filters, int
 		return 0;
 }
 
+#ifdef WIN32
+static int each_file_dirw(const wchar_t *path, struct file_type_filter *filters, int flags) {
+	WIN32_FIND_DATA fdata;
+	HANDLE h = FindFirstFile(path, &fdata);
+	if(h == INVALID_HANDLE_VALUE) return -1;
+	do {
+		wchar_t rpath[MAX_PATH];
+		if(path[0])
+			swprintf(file_path, MAX_PATH, L"%s\\%s", path, fdata.cFileName);
+		else
+			swprintf(file_path, MAX_PATH, L"%s", fdata.cFileName);
+		each_filew(rpath, filters, flags);
+	} while (FindNextFile(h, &fdata) != 0);
+	FindClose(h);
+}
+#endif
+
 #define FILL_PATH_INFO(path) \
 	p.file_name = (char *)path; \
 	char *file_dirname_str = strdup(path); \
@@ -514,6 +531,20 @@ static int each_file_dir(const char *path, struct file_type_filter *filters, int
 	p.file_basename = basename(file_basename_str); \
 	char *file_ext_str = strdup(p.file_basename); \
 	char *file_ext = strrchr(file_ext_str, '.'); \
+	if(file_ext) { \
+		*file_ext = 0; \
+		p.file_ext = file_ext[1] ? file_ext + 1 : 0; \
+	} \
+	p.file_base = file_ext_str;
+
+#define FILL_PATH_INFOW(path) \
+	p.file_name = (wchar_t *)path; \
+	wchar_t *file_dirname_str = _wcsdup(path); \
+	p.file_dirname = dirname(file_dirname_str); \
+	wchar_t *file_basename_str = _wcsdup(path); \
+	p.file_basename = basename(file_basename_str); \
+	wchar_t *file_ext_str = _wcsdup(p.file_basename); \
+	wchar_t *file_ext = strrchr(file_ext_str, '.'); \
 	if(file_ext) { \
 		*file_ext = 0; \
 		p.file_ext = file_ext[1] ? file_ext + 1 : 0; \
@@ -568,7 +599,78 @@ static int each_file_zip(const char *path, struct file_type_filter *filters, int
 	zip_close(z);
 	return 0;
 }
-#endif
+
+#ifdef WIN32
+wchar_t *_wbasename(wchar_t* path) {
+	wchar_t* base = wcspbrk(path, L"\\/");
+	return base ? base + 1 : path;
+}
+
+wchar_t* _wdirname(wchar_t* path) {
+	wchar_t* last_backslash = wcspbrk(path, L"\\/");
+
+	if(last_backslash != NULL) {
+		if(last_backslash == path)
+			*(last_backslash + 1) = L'\0';
+		else
+			*last_backslash = L'\0';
+	} else wcscpy(path, L".");
+
+	return path;
+}
+
+static int each_file_zipw(const wchar_t *path, struct file_type_filterw *filters, int flags) {
+	(void)flags;
+	int fd = _wfopen(path, L"rb");
+	if(!fd) return errno;
+	int err;
+	zip_t *z = zip_fdopen(fd, 0, &err);
+	if(!z) return err;
+	int num_entries = zip_get_num_entries(z, 0);
+	if(num_entries < 0) {
+		zip_close(z);
+		return -1;
+	}
+	struct path_infow p;
+	p.zip_file_name = (wchar_t *)path;
+	wchar_t *zip_dirname_str = _wcsdup(path);
+	p.zip_file_dirname = _wdirname(zip_dirname_str);
+	wchar_t *zip_basename_str = _wcsdup(path);
+	p.zip_file_base = _wbasename(zip_basename_str);
+	wchar_t *zip_ext = wcsrchr(p.zip_file_base, L'.');
+	if(zip_ext) *zip_ext = 0;
+
+	for(int j = 0; j < num_entries; j++) {
+		zip_stat_t st;
+		zip_stat_index(z, j, ZIP_STAT_NAME | ZIP_STAT_SIZE, &st);
+		int l = strlen(st.name)
+		wchar_t *stname = malloc((l + 1) * sizeof(wchar_t));
+		if(!stname) return -1;
+		for(int k = 0; k < l; k++)
+			stname[k] = st.name[k];
+		const wchar_t *ext = wcsrchr(st.name, L'.');
+		if(!ext || !ext[1]) continue;
+		for(struct file_type_filterw *f = filters; f->ext; f++) {
+			if(_wcsicmp(ext, f->ext)) continue;
+			struct zip_file_stream s;
+			int r = zip_file_stream_init_index(&s, z, j);
+			if(r) return r;
+			FILL_PATH_INFOW(st.name);
+			r = f->file_cb(&p, (struct stream *)&s, f->user_data);
+			FREE_PATH_INFO();
+			stream_close((struct stream *)&s);
+			if(r) return r;
+			break;
+		}
+	}
+	free(zip_basename_str);
+	free(zip_dirname_str);
+	zip_close(z);
+	return 0;
+}
+#endif /* WIN32 */
+
+#endif /* HAVE_LIBZIP */
 
 static int each_file_file(const char *path, const char *ext, struct file_type_filter *filters, int flags) {
 	for(struct file_type_filter *f = filters; f->ext; f++) {
@@ -594,6 +696,32 @@ static int each_file_file(const char *path, const char *ext, struct file_type_fi
 	return 1;
 }
 
+#ifdef WIN32
+static int each_file_filew(const wchar_t *path, const wchar_t *ext, struct file_type_filterw *filters, int flags) {
+	for(struct file_type_filterw *f = filters; f->ext; f++) {
+		if(_wcsicmp(ext, f->ext)) continue;
+		struct path_infow p;
+		p.zip_file_name = p.zip_file_base = p.zip_file_dirname = 0;
+		if(flags & EF_OPEN_STREAM) {
+			struct file_stream s;
+			int r = file_stream_initw(&s, path, "rb");
+			if(r) return r;
+			FILL_PATH_INFOW(path);
+			r = f->file_cb(&p, (struct stream *)&s, f->user_data);
+			FREE_PATH_INFO();
+			stream_close((struct stream *)&s);
+			if(r) return r;
+		} else {
+			FILL_PATH_INFOW(path);
+			f->file_cb(&p, 0, f->user_data);
+			FREE_PATH_INFO();
+		}
+		return 0;
+	}
+	return 1;
+}
+#endif
+
 int each_file(const char *path, struct file_type_filter *filters, int flags) {
 	struct stat st;
 	int r = stat(path, &st);
@@ -612,3 +740,24 @@ int each_file(const char *path, struct file_type_filter *filters, int flags) {
 	}
 	return 0;
 }
+
+#ifdef WIN32
+int each_filew(const wchar_t *path, struct file_type_filterw *filters, int flags) {
+	struct _stat st;
+	int r = _wstat(path, &st);
+	if(r < 0) return errno;
+	if(S_ISDIR(st.st_mode) && (flags & EF_RECURSE_DIRS)) {
+		return each_file_dirw(path, filters, flags);
+	} else {
+		const wchar_t *ext = wcsrchr(path, L'.');
+		if(ext) {
+#ifdef HAVE_LIBZIP
+			if(!_wcsicmp(ext, L".zip") && (flags & EF_RECURSE_ARCHIVES) && (flags & EF_OPEN_STREAM))
+				return each_file_zipw(path, filters, flags);
+#endif
+			return each_file_filew(path, ext, filters, flags);
+		}
+	}
+	return 0;
+}
+#endif
