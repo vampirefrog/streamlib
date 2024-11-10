714bb518360d7a28178e7f5c0f9b4049b7948712
vampi <vampi@vampi.tech>
2024-11-10 22:08:59 +0200

    remove old comments

--- mem_stream.h
+++ mem_stream.h
@@ -6,10 +6,6 @@
 
 #include "stream_base.h"
 
-/**
- * @struct mem_stream
- * @brief Memory stream structure for handling in-memory data streams.
- */
 struct mem_stream {
 	struct stream stream; /**< Base stream structure */
 	void *data; /**< Pointer to the data buffer */
@@ -22,19 +18,5 @@ struct mem_stream {
 #endif
 };
 
-/**
- * @brief Initialize a memory stream.
- * @param stream Pointer to the memory stream object.
- * @param existing_data Pointer to the existing data buffer. If NULL, data_len bytes are allocated.
- * @param data_len Length of the data.
- * @return Status code.
- */
 int mem_stream_init(struct mem_stream *stream, void *existing_data, size_t data_len, int stream_flags);
-
-/**
- * @brief Create a memory stream.
- * @param existing_data Pointer to the existing data buffer. If NULL, data_len bytes are allocated.
- * @param existing_data_len Length of the data.
- * @return Pointer to the created memory stream object.
- */
 struct stream *mem_stream_new(void *existing_data, size_t existing_data_len, int stream_flags);
